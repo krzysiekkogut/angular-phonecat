c2fcb65aa0809e4832476523337b8378d1fba659
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-03-11 10:24:26 +0100

    Update phone service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { UpgradeModule } from "@angular/upgrade/static";
+import { Phone } from "./core/phone/phone.service";
 
 @NgModule({
-  imports: [BrowserModule, UpgradeModule]
+  imports: [BrowserModule, UpgradeModule, HttpModule],
+  providers: [Phone]
 })
 export class AppModule {
   constructor(private upgrade: UpgradeModule) {}
